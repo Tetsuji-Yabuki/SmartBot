7aea340931096ae879d49a8c3dbdda0610e7413e
Tetsuji-Yabuki <t.yabuki@gloops.com>
2016-11-08 17:56:32 +0900

    first commit

---
+++ SmartBot/Controllers/MessagesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using SmartBot.Domain.Model.Command;
+
+namespace SmartBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (ActivityTypes.Message.Equals(activity.Type))
+            {
+                var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+                var command = CommandFactory.Create(activity.Text);
+
+                var result = command.Execute();
+
+                var message = new StringBuilder();
+                message.AppendLine($"Command[{command.CommandText}]");
+                message.AppendLine($"Args[{string.Join(",", command.Arguments)}]");
+                message.AppendLine($"Result[{nameof(result.EndCode)}]");
+                message.AppendLine($"Message[{result.Message}]");
+
+                var reply = activity.CreateReply(message.ToString());
+
+                await connector.Conversations.ReplyToActivityAsync(reply);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+
+            return response;
+        }
+    }
+}
