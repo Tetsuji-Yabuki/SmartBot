608ce314dfb9c85c8a6cd492aa5669b7de56bcd9
Tetsuji-Yabuki <t.yabuki@gloops.com>
2016-11-09 19:12:26 +0900

    テストコード追加

---
+++ SmartBot.Domain.Test/ChatBotCommandTest.cs
@@ -0,0 +1,19 @@
+﻿
+using SmartBot.Domain.Model.Command;
+using Xunit;
+
+namespace SmartBot.Domain.Test
+{
+    public class ChatBotCommandTest
+    {
+        [Fact]
+        public void ExecuteTest()
+        {
+            var cmd = CommandFactory.Create("chat:test");
+
+            var result = cmd.Execute();
+
+            Assert.Equal(0, (int)result.EndCode);
+        }
+    }
+}
